95da3125406e69029edadc8455227fa5955a3c35
FabrizioFavia <personal.favia@gmail.com>
2023-06-29 16:16:08 +0200

    create trains table

---
+++ database/migrations/2023_06_29_140914_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company');
+            $table->string('departure_station');
+            $table->string('arrival_station');
+            $table->char('departure_time', 5);
+            $table->char('arrival_time', 5);
+            $table->char('train_code', 30);
+            $table->smallInteger('coaches_number');
+            $table->boolean('on_time');
+            $table->boolean('cancelled');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
